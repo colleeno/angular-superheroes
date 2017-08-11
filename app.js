88d425b1f58579602eaa58b680e58cbcb802b1dc
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-08-11 18:22:55 -0400

    added some styles

--- app.js
+++ app.js
@@ -5,10 +5,12 @@
   .controller ('SuperheroController', [SuperheroController])
     function SuperheroController () {
       this.superheroes = [
-        {name: 'name1', phone_number: '111-111-1111', comics_affiliation: 'marvel', image_url: 'http://images5.fanpop.com/image/photos/29500000/GamesRadar-Puppy-puppies-29566786-200-200.jpg', selected: false},
-        {name: 'name2', phone_number: '222-222-2222', comics_affiliation: 'dc', image_url: 'http://www.animal-photography.com/thumbs/3_rough_collie_puppies_sitting_together~AP-BDTEU4-TH.jpg', selected: false},
-        {name: 'name3', phone_number: '333-333-3333', comics_affiliation: 'marvel', image_url: 'http://adogbreeds.com/wp-content/uploads/2012/12/Pembroke-Welsh-Corgi-Puppies-Lying-Down-200x200.jpg', selected: false},
-        {name: 'name4', phone_number: '444-444-4444', comics_affiliation: 'dc', image_url: 'http://fourmuddypaws.com/wp-content/uploads/2014/05/bulldog-puppies-playing-e1401294368290.jpg', selected: false}
+        {name: 'Batman', phone_number: '111-111-1111', comics_affiliation: 'dc', image_url: 'http://i.imgur.com/kFD5apF.png', selected: false},
+        {name: 'Hulk', phone_number: '222-222-2222', comics_affiliation: 'marvel', image_url: 'http://i.imgur.com/X6sh5Jx.png', selected: false},
+        {name: 'Spider-Man', phone_number: '333-333-3333', comics_affiliation: 'marvel', image_url: 'http://i.imgur.com/bGByRfb.png', selected: false},
+        {name: 'Wonder Woman', phone_number: '444-444-4444', comics_affiliation: 'dc', image_url: 'http://i.imgur.com/ckMqnxq.png', selected: false},
+        {name: 'Aquaman', phone_number: '555-555-5555', comics_affiliation: 'dc', image_url: 'http://i.imgur.com/fEjVJkp.png', selected: false},
+        {name: 'Superman', phone_number: '666-666-6666', comics_affiliation: 'dc', image_url: 'http://i.imgur.com/eRHtrCy.png', selected: false}
       ]
       this.selectHero = function (hero) {
           // hero.selected = !hero.selected
